7c7b773d31c84411a91502eacbbb5484db292af5
SzpontRank Dev <dev@szpontrank.eu>
2026-09-03 05:14:12 +0000

    Uproszczenie /xdd - jedna czysta ilustracja telefonu (logo, proste bloki zamiast tekstu z haslami, uproszczone menu na dole) zamiast pasków z konkretnymi funkcjami

--- src/ReklamaDemo.jsx
+++ src/ReklamaDemo.jsx
@@ -1,79 +1,32 @@
-import { IkonaKorona, IkonaMoneta, IkonaGlobus, IkonaOgien, IkonaDom, IkonaGrupa, IkonaCzat, IkonaOsoba } from './Ikony'
-import { OdznakaRangi } from './rangi'
-
-function IlustracjaMenu() {
-  return (
-    <div className="xdd-telefon">
-      <div className="xdd-telefon-ekran">
-        <div className="xdd-telefon-logo">
-          <img src="/brand/emblem.png" alt="" />
-        </div>
-        <div className="xdd-telefon-menu">
-          <div className="xdd-telefon-pozycja xdd-telefon-aktywna">
-            <IkonaDom rozmiar={22} />
-            <span>Dom</span>
-          </div>
-          <div className="xdd-telefon-pozycja">
-            <IkonaKorona rozmiar={22} />
-            <span>Misje</span>
-          </div>
-          <div className="xdd-telefon-pozycja">
-            <IkonaGrupa rozmiar={22} />
-            <span>Rankingi</span>
-          </div>
-          <div className="xdd-telefon-pozycja">
-            <IkonaCzat rozmiar={22} />
-            <span>Znajomi</span>
-          </div>
-          <div className="xdd-telefon-pozycja">
-            <IkonaOsoba rozmiar={22} />
-            <span>Profil</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+import { IkonaKorona, IkonaMoneta, IkonaDom, IkonaGrupa, IkonaCzat, IkonaOsoba } from './Ikony'
 
 export default function ReklamaDemo() {
   return (
-    <div className="xdd-scena">
-      <div className="xdd-pasek xdd-pasek-logo">
-        <span className="xdd-marka">SZPONTRANK</span>
-      </div>
+    <div className="xdd-tlo">
+      <div className="xdd-telefon xdd-telefon-duzy">
+        <div className="xdd-telefon-ekran">
+          <div className="xdd-ekran-naglowek">
+            <img src="/brand/emblem.png" alt="" />
+            <span>SzpontRank</span>
+          </div>
 
-      <div className="xdd-pasek xdd-pasek-ilustracja">
-        <IlustracjaMenu />
-      </div>
+          <div className="xdd-ekran-blok xdd-ekran-blok-zlota" />
+          <div className="xdd-ekran-blok xdd-ekran-blok-duzy">
+            <IkonaKorona rozmiar={22} />
+          </div>
+          <div className="xdd-ekran-blok xdd-ekran-blok-duzy">
+            <IkonaMoneta rozmiar={22} />
+          </div>
+          <div className="xdd-ekran-blok" />
 
-      <div className="xdd-pasek xdd-pasek-a">
-        <IkonaGlobus rozmiar={36} />
-        <span>CODZIENNE PYTANIE DNIA</span>
-      </div>
-
-      <div className="xdd-pasek xdd-pasek-b">
-        <IkonaKorona rozmiar={36} />
-        <span>POJEDYNKI 1 NA 1</span>
-      </div>
-
-      <div className="xdd-pasek xdd-pasek-c">
-        <IkonaMoneta rozmiar={36} />
-        <span>ZBIERAJ COINY</span>
-      </div>
-
-      <div className="xdd-pasek xdd-pasek-d">
-        <OdznakaRangi klucz="diament" rozmiar={40} />
-        <span>PNIJ SIĘ W RANGACH</span>
-      </div>
-
-      <div className="xdd-pasek xdd-pasek-e">
-        <IkonaOgien rozmiar={36} />
-        <span>BUDUJ STREAK</span>
-      </div>
-
-      <div className="xdd-pasek xdd-pasek-cta">
-        <span className="xdd-cta-duze">ZDOBĄDŹ RANGĘ.</span>
-        <span className="xdd-cta-male">Za darmo. Już dziś.</span>
+          <div className="xdd-telefon-menu">
+            <div className="xdd-telefon-pozycja xdd-telefon-aktywna"><IkonaDom rozmiar={20} /></div>
+            <div className="xdd-telefon-pozycja"><IkonaKorona rozmiar={20} /></div>
+            <div className="xdd-telefon-pozycja"><IkonaGrupa rozmiar={20} /></div>
+            <div className="xdd-telefon-pozycja"><IkonaCzat rozmiar={20} /></div>
+            <div className="xdd-telefon-pozycja"><IkonaOsoba rozmiar={20} /></div>
+          </div>
+        </div>
       </div>
     </div>
   )
